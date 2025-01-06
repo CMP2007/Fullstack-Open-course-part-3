e04cfa560f8238103acd482ba3bf0147d6ae2155
PucheCarlos <puchecarlos00@gmail.com>
2025-01-05 23:00:42 -0400

    exercise 3.8

--- index.js
+++ index.js
@@ -66,7 +66,20 @@ app.delete('/api/persons/:id', (request, response) => {
 
 
 app.use(express.json())
-app.use(morgan('tiny'))
+morgan.format('personalized', function (tokens, req, res) {
+  return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'),
+      '-',
+      tokens['response-time'](req, res),
+      'ms',
+      JSON.stringify(req.body)
+  ].join(' ')
+});
+
+app.use(morgan('personalized'));
 
 
 app.post(`/api/persons`, (request, response) => {
